f16a12e7bf7828c67a1eb078295716bcce2aca21
Alvaro Tuso <alvarotuso@hotmail.com>
2024-12-28 14:46:06 +0000

    part 2

--- aoc2024/src/bin/question_01.rs
+++ aoc2024/src/bin/question_01.rs
@@ -1,7 +1,7 @@
 use aoc2024::client::AocClient;
 use aoc2024::errors::AocError;
 use futures::TryStreamExt;
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashMap};
 
 #[tokio::main]
 async fn main() -> Result<(), AocError> {
@@ -9,6 +9,8 @@ async fn main() -> Result<(), AocError> {
     let mut stream = Box::pin(aoc_client.get_input_stream(2024, 1).await);
     let mut heap_list_1 = BinaryHeap::new();
     let mut heap_list_2 = BinaryHeap::new();
+    let mut l2_occurrences = HashMap::new();
+    let mut l1_numbers = Vec::new();
     while let Some(line) = stream.try_next().await? {
         let parts = line
             .split_ascii_whitespace()
@@ -23,22 +25,32 @@ async fn main() -> Result<(), AocError> {
         }
         let parse_err = Err(AocError::Error(format!("failed to parse line: {}", line)));
         if let Ok(l1) = parts[0] {
+            l1_numbers.push(l1);
             heap_list_1.push(l1);
         } else {
             return parse_err;
         }
         if let Ok(l2) = parts[1] {
+            l2_occurrences
+                .entry(l2)
+                .and_modify(|c| *c += 1)
+                .or_insert(1);
             heap_list_2.push(l2);
         } else {
             return parse_err;
         }
     }
-    let mut total = 0;
+    let mut total_part_1 = 0;
     while let Some(l1) = heap_list_1.pop() {
         let l2 = heap_list_2.pop().unwrap();
-        total += (l1 - l2).abs();
+        total_part_1 += (l1 - l2).abs();
     }
-    println!("Part 1: {}", total);
+    println!("Part 1: {}", total_part_1);
 
+    let mut total_part_2 = 0;
+    for l1 in l1_numbers {
+        total_part_2 += l1 * l2_occurrences.get(&l1).unwrap_or(&0);
+    }
+    println!("Part 2: {}", total_part_2);
     Ok(())
 }
