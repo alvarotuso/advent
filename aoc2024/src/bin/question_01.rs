a89bebcdaffbae3699b6616c88b409056ac2c7fc
Alvaro Tuso <alvarotuso@hotmail.com>
2024-12-28 02:57:15 +0000

    getting started

--- aoc2024/src/bin/question_01.rs
+++ aoc2024/src/bin/question_01.rs
@@ -1,8 +1,13 @@
-use tokio;
-
+use futures::TryStreamExt;
+use aoc2024::client::AocClient;
 use aoc2024::errors::AocError;
 
 #[tokio::main]
 async fn main() -> Result<(), AocError> {
+    let aoc_client = AocClient::new(reqwest::Client::new());
+    let mut s = Box::pin(aoc_client.get_input_stream(2024, 1).await);
+    while let Some(line) =  s.try_next().await? {
+        println!("{}", line);
+    }
     Ok(())
 }
--- aoc2024/src/client.rs
+++ aoc2024/src/client.rs
@@ -1,32 +1,22 @@
+use std::env;
+use std::env::VarError;
+use async_stream::try_stream;
 use bytes::Bytes;
+use futures::{Stream, TryStreamExt};
 use reqwest;
 use thiserror::Error;
-use futures::stream::IntoAsyncRead;
-use futures::{AsyncRead, Stream, TryStreamExt};
-use tokio::io::AsyncBufRead;
-use tokio_util::compat::FuturesAsyncReadCompatExt;
-use tokio_util::io::StreamReader;
+
+const SESSION_COOKIE_ENV: &str = "AOC_SESSION_COOKIE";
 
 #[derive(Debug, Error)]
 pub enum AocClientError {
     #[error("request error: {0}")]
     ReqwestError(#[from] reqwest::Error),
+    #[error("missing environment variable error: {0}")]
+    VarError(#[from] VarError),
 }
 
-
-type InputStream = dyn Stream<Item=reqwest::Result<Bytes>>;
-
-struct  InputIterator {
-    stream: Box<InputStream>,
-}
-
-impl InputIterator {
-    fn new(stream: impl Stream<Item=reqwest::Result<Bytes>>) -> Self {
-        Self { stream: Box::new(stream) }
-    }
-}
-
-struct AocClient {
+pub struct AocClient {
     client: reqwest::Client,
 }
 
@@ -35,9 +25,29 @@ impl AocClient {
         AocClient { client }
     }
 
-    pub async fn get_input_reader(&self, year: i8, question: i8) -> Result<impl AsyncBufRead, AocClientError> {
+    async fn get_input_stream_bytes(&self, year: i16, question: i8) -> Result<impl Stream<Item=Result<Bytes, reqwest::Error>>, AocClientError> {
         let url = format!("https://adventofcode.com/{}/day/{}/input", year, question);
-        let stream = self.client.get(url).send().await?.bytes_stream();
-        Ok(StreamReader::new(stream))
+        let session = env::var(SESSION_COOKIE_ENV)?;
+        Ok(self.client.get(url).header("Cookie", format!("session={}", session)).send().await?.bytes_stream())
+    }
+    
+    pub async fn get_input_stream(&self, year: i16, question: i8) -> impl Stream<Item=Result<String, AocClientError>> + use<'_> {
+        try_stream! {
+            let mut s = self.get_input_stream_bytes(year, question).await?;
+            let mut line = String::new();
+            while let Some(item) = s.try_next().await? {
+                for b in item {
+                    if b == b'\n' {
+                        yield line;
+                        line = String::new();
+                    } else {
+                        line.push(char::from(b));
+                    }
+                }
+            }
+            if !line.is_empty() {
+                yield line
+            }
+        }
     }
 }
